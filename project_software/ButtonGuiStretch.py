3ced52607531af7d4092bfcc9c4f0b077fec8639
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-02-11 12:22:05 +0200

    ButtonGuiStretch.py

--- project_software/ButtonGuiStretch.py
+++ project_software/ButtonGuiStretch.py
@@ -7,6 +7,8 @@ def window():
     app = QApplication(sys.argv)
     win = QWidget()
 
+    #---------------------------------------------------
+    # Adding Start, Pause & Stop buttons
     start_btn = QPushButton("Start")
     pause_btn = QPushButton("Pause")
     stop_btn = QPushButton("Stop")
@@ -19,11 +21,32 @@ def window():
     box.addStretch()
     box.addWidget(stop_btn)
 
-    win.setLayout(box)
-
     start_btn.clicked.connect(start_clicked)
     QObject.connect(pause_btn,SIGNAL("clicked()"),pause_clicked)
     QObject.connect(stop_btn,SIGNAL("clicked()"),stop_clicked)
+
+    #---------------------------------------------------
+    # Adding inputs for XY coordinates
+    fbox = QFormLayout()
+    hbox = QHBoxLayout()
+
+    l_x=QLabel("X_target =")
+    l_y=QLabel("Y_target =")
+
+    addx = QLineEdit()
+    addy = QLineEdit()
+
+    hbox.addWidget(addx)
+    fbox.addRow(l_x,hbox)
+
+    hbox.addWidget(addy)
+    fbox.addRow(l_y,hbox)
+
+    
+
+
+
+    win.setLayout(fbox)
 
     win.setWindowTitle("PyQt")
     win.show()
