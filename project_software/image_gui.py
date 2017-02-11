8289ea9dad2ca4db9f96b5e927f9b8448f8aed6d
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-02-11 15:46:31 +0200

    image_gui.py

--- project_software/image_gui.py
+++ project_software/image_gui.py
@@ -3,85 +3,53 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 
+
 def window():
     app = QApplication(sys.argv)
     win = QWidget()
-
-    #---------------------------------------------------
-    # Adding Start, Pause & Stop buttons
-    fbox = QFormLayout()
-    hbox = QHBoxLayout()
-    vbox = QVBoxLayout()
     
-    start_btn = QPushButton("Start")
-    pause_btn = QPushButton("Pause")
-    stop_btn = QPushButton("Stop")
-
+    l1=QLabel()
+    l2=QLabel()
+    l3=QLabel()
+    l4=QLabel()
     
-
-    hbox.addWidget(start_btn)
-    hbox.addStretch()
-    hbox.addWidget(pause_btn)
-    hbox.addStretch()
-    hbox.addWidget(stop_btn)
+    l1.setText("Hello World")
+    l4.setText("<A href='www.TutorialsPoint.com'>TutorialsPoint</a>")
+    l2.setText("<a href='#'>welcome to Python GUI Programming</a>")
     
-    
-
-    start_btn.clicked.connect(start_clicked)
-    QObject.connect(pause_btn,SIGNAL("clicked()"),pause_clicked)
-    QObject.connect(stop_btn,SIGNAL("clicked()"),stop_clicked)
-
-    vbox.addStretch()
-    '''
-    #---------------------------------------------------
-    # Adding inputs for XY coordinates
-    
-    
-
-    #l_x=QLabel("X_target =")
-    #l_y=QLabel("Y_target =")
-
-    addx = QLineEdit()
-    addy = QLineEdit()
-
-    #hbox.addWidget(addx)
-    #fbox.addRow(l_x,hbox)
-
-    #hbox.addWidget(addy)
-    #fbox.addRow(l_y,hbox)
-    
-
-    vbox.addStretch()
-
-    fbox.addRow(QLabel("X_target ="),addx)
-    fbox.addRow(QLabel("Y_target ="),addy)
-    
-    vbox.addStretch()
-
-'''
-    #---------------------------------------------------
-    # Adding image
-    l3 = QLabel()
+    l1.setAlignment(Qt.AlignCenter)
+    l3.setAlignment(Qt.AlignCenter)
+    l4.setAlignment(Qt.AlignRight)
     l3.setPixmap(QPixmap("detected circles.jpg"))
+    
+    vbox=QVBoxLayout()
+    
+    vbox.addWidget(l1)
+    vbox.addStretch()
+    vbox.addWidget(l2)
     vbox.addStretch()
     vbox.addWidget(l3)
-
-
-    win.setLayout(fbox)
-
-    win.setWindowTitle("PyQt")
+    vbox.addStretch()
+    vbox.addWidget(l4)
+    
+    l1.setOpenExternalLinks(True)
+    l4.linkActivated.connect(clicked)
+    l2.linkHovered.connect(hovered)
+    l1.setTextInteractionFlags(Qt.TextSelectableByMouse)
+    win.setLayout(vbox)
+    
+    win.setWindowTitle("QLabel Demo")
     win.show()
     sys.exit(app.exec_())
 
 
-def start_clicked():
-	print "Start clicked"
 
-def pause_clicked():
-	print "Pause clicked"
-
-def stop_clicked():
-	print "Stop clicked"
+def hovered():
+    print "hovering"
+def clicked():
+    print "clicked"
 
 if __name__ == '__main__':
     window()
+
+    
