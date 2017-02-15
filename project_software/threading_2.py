f7eaf8716ee877b2918853258e72e3d517ab777e
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-02-15 20:59:10 +0200

    threading2.py

--- project_software/threading_2.py
+++ project_software/threading_2.py
@@ -3,7 +3,7 @@ import sys
 
 from PyQt4 import QtCore
 from PyQt4 import QtGui
-from PyQt4.QtCore import (Qt, SIGNAL)
+from PyQt4.QtCore import *#(Qt, SIGNAL)
 from PyQt4.QtGui import (QApplication, QDialog, QHBoxLayout, QLabel, QPushButton)
 
 #from PyQt4.QtCore import SIGNAL
