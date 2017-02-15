3a988409f2140a844d87e6929b0e45583cae356a
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-02-15 18:43:39 +0200

    threading.py

--- project_software/threading.py
+++ project_software/threading.py
@@ -13,10 +13,13 @@ class Form(QDialog):
         #Allow Qt to set up the object.
         super(Form, self).__init__(parent)
 
+        layout = QVBoxLayout()
+
         self.process_btn=QtGui.QPushButton()
         self.process_btn.setText("Process")
         self.connect(self.process_btn,SIGNAL("clicked()"),self.processData)
-
+        layout.addWidget(self.process_btn)
+        
         self.workrThread = WorkrThread()
 
     def processData(self):
