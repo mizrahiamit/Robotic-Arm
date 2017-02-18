cea15aa689d7ac596e6d55967e1743420f74748d
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-02-18 16:03:00 +0200

    detect_three_circles

---
+++ project_software/detect_three_circles_coordinates.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+from huogh_transform_function import *
+
+
+def main():
+    # Take a image
+    image = cv2.imread('two circles.jpg')
+    image2 = image.copy()
+    #---------Detect Red Objects-------------
+    r_mask = get_color_mask(image,"red")
+    
+    #---------Detect Red circle-------------
+
+    circles = return_circles_values(r_mask)
+    
+    if circles is None:
+        print "No circle found"
+    else:
+
+        image2 = draw_circle(image2,circles)
+        print circles
+        
+        cv2.imwrite('detected red circles.jpg',image2)
+    #---------Detect Green Objects-------------
+    b_mask = get_color_mask(image,"green")
+
+    #---------Detect Green circle-------------
+    circles = return_circles_values(b_mask)
+    
+    if circles is None:
+        print "No circle found"
+
+    else:
+        image2 = draw_circle(image2,circles)
+        print circles
+        
+        cv2.imwrite('detected green circles.jpg',image2)
+    #---------Detect Yellow Objects-------------
+    b_mask = get_color_mask(image,"yellow")
+
+    #---------Detect Yellow circle-------------
+    circles = return_circles_values(b_mask)
+    
+    if circles is None:
+        print "No circle found"
+
+    else:
+        image2 = draw_circle(image2,circles)
+        print circles
+        
+        cv2.imwrite('detected yellow circles.jpg',image2)
+
+if (__name__ == "__main__"):
+	main()
