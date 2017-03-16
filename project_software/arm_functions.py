59f784b40dbb88b2e908350c6335021cff387ea7
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-03-16 13:19:53 +0200

    debug

--- project_software/arm_functions.py
+++ project_software/arm_functions.py
@@ -13,34 +13,34 @@ def cal_deviation(arm_wrist_pos, x_dest, y_dest):
 	return math.hypot(arm_wrist_pos[0] - x_dest, arm_wrist_pos[1] - y_dest)
 
 
-def cal_next_move(_deviation, _distance, _wrist_pos, _shoulder_pos, _x_pos, _y_pos):
+def cal_next_move(_distance, _wrist_pos, _shoulder_pos, _x_pos, _y_pos):
 	cover_radius = math.hypot(_wrist_pos[0] - _shoulder_pos[0], _wrist_pos[1] - _shoulder_pos[1]) #The distance between the wirst to the shoulder
 	
-	if (cover_radius > (_deviation)):
+	if (cover_radius > (_distance+5)):
 		print cover_radius," > ",_deviation
 		m1_change =  0
 		m2_change = -0.5 # -9 degrees
 		print "motor2 -9 deg"
 
-	elif (cover_radius < (_deviation)):
+	elif (cover_radius < (_distance-5)):
 		print cover_radius," < ",_deviation
 		m1_change =  0
 		m2_change = +0.25 # +9 degrees ********************************
 		print "motor2 +4.5 deg"
 
-	elif (_wrist_pos[1] < (_y_pos)):
+	elif (_wrist_pos[1] < (_y_pos-5)):
 		print _wrist_pos[1]," < ",_y_pos
 		m1_change = -0.5 # +9 degrees
 		m2_change = 0
 		print "motor1 +9 deg"
 
-	elif (_wrist_pos[1] > (_y_pos)):
+	elif (_wrist_pos[1] > (_y_pos+5)):
 		print _wrist_pos[1]," > ",_y_pos
 		m1_change = +0.5 # -9 degrees
 		m2_change = 0
 		print "motor1 -9 deg"
 
-	elif (_wrist_pos[0] < _x_pos):
+	elif (_wrist_pos[0] < _x_pos-5):
 		print _wrist_pos[0]," < ",_x_pos
 		m1_change = -0.25 # - 4.5 degrees
 		m2_change = 0
