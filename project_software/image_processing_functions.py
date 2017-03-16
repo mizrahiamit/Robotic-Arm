d7a71c019180fdbf1e6559ace91c7b6de8b80f0d
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-03-16 10:32:52 +0200

    debug

--- project_software/image_processing_functions.py
+++ project_software/image_processing_functions.py
@@ -29,40 +29,40 @@ def get_color_mask(image,color):
 
     elif color =='blue':
         # define range of blue color in HSV
-        '''
+        
         lower_blue = np.array([90,30,0])
         upper_blue = np.array([115,255,255])
         '''
         #home
         lower_blue = np.array([110,50,50])
         upper_blue = np.array([130,255,255])
-        
+        '''
         
         # Threshold the HSV image to get only blue colors
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
     elif color == 'green':
         # define range of green color in HSV
-        '''
+        
         lower_green = np.array([50,90,90])
         upper_green = np.array([70,255,255])
         '''
         #home
         lower_green = np.array([40,50,50])
         upper_green = np.array([60,255,255])
-        
+        '''
         
         # Threshold the HSV image to get only green colors
         mask = cv2.inRange(hsv, lower_green, upper_green)
     elif color =='yellow':
         # define range of yellow color in HSV
-        '''
+        
         lower_yellow = np.array([20,230,60])
         upper_yellow = np.array([40,255,255])
         '''
         #home
         lower_yellow = np.array([20,100,100])
         upper_yellow = np.array([30,255,255])
-        
+        '''
         
         # Threshold the HSV image to get only yellow colors
         mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
