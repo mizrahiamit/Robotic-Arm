2e52594b29b49535c0a953ee995738c609f7006a
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-03-16 13:25:34 +0200

    debug

--- project_software/main.py
+++ project_software/main.py
@@ -318,7 +318,7 @@ class Form(QWidget):
                 #/////////////////////////////////////////////////
                 print "check success"
                 self._deviation = cal_deviation(self._wrist_pos, self._x_pos, self._y_pos)
-                if (self._deviation < 5):
+                if (self._deviation < 10):
                     self.status_msg.setText("Success")
                     time.sleep(30)
                     self.stop_clicked
