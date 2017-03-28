a46ad284414b04d571f28c32d7d7f309246a3374
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-03-28 21:21:41 +0300

    debug

--- project_software/main_functions.py
+++ project_software/main_functions.py
@@ -45,17 +45,18 @@ def take_new_picture(_x_pos,_y_pos,cam):
     # "Decode" the image from the array, preserving colour
     image = cv2.imdecode(data, 1)
     '''
-    data = cam.read()[1]
-    image = cv2.imdecode(data, 1)
-    newImage = image.copy()
+    image = cam.read()[1]
+    
+    
     # OpenCV returns an array with data in BGR order. If you want RGB instead
     if (_x_pos!=0) or (_y_pos!=0):
         cv2.line(image,(_x_pos+10,_y_pos),(_x_pos-10,_y_pos),(0,0,255),1)
         cv2.line(image,(_x_pos,_y_pos+10),(_x_pos,_y_pos-10),(0,0,255),1)
 
     cv2.imwrite("Test Image.jpg",image)
-        
-    return newImage
+    
+    return image    
+    #return newImage
 
 def print_miss(_shoulder_miss,_elbow_miss,_wrist_miss):
     print "Miss detect yellow circel : ",_shoulder_miss
